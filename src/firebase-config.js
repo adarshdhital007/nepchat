7e9c343cf3f148d573db91a88faa74fa2c0de5f8
Adarsh Dhital Khatri <90372904+adarshdhital007@users.noreply.github.com>
2023-09-10 12:31:36 -0700

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth , GoogleAuthProvider} from "firebase/auth"
-import {getFirestore} from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDHGENiZtZ38NwNJ6YgSO5UaVtWZxFof90",
-  authDomain: "realtimechat-155fd.firebaseapp.com",
-  projectId: "realtimechat-155fd",
-  storageBucket: "realtimechat-155fd.appspot.com",
-  messagingSenderId: "1015463024281",
-  appId: "1:1015463024281:web:9f1ddfaa3232ebdccf8a08",
+  apiKey: "AIzaSyCfVSfMu10dEWkP_DIkpm73bmQcNramstc",
+  authDomain: "nepchat-4102e.firebaseapp.com",
+  projectId: "nepchat-4102e",
+  storageBucket: "nepchat-4102e.appspot.com",
+  messagingSenderId: "154802142628",
+  appId: "1:154802142628:web:42cc53ec67cedddc4ed531",
+  measurementId: "G-ENQW4C4JZV"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth=getAuth(app);
-export const provider  = new GoogleAuthProvider();
-export const db=getFirestore(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
